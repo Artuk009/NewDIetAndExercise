0056771a38c3b2df07d7e220660c2a15a1156b39
Brent Artuch <115948736+Artuk009@users.noreply.github.com>
2023-07-15 00:53:01 -0400

    Deleted meals file for lack of use

--- Console_Application/diet_db/meals.py
+++
@@ -1,9 +0,0 @@
-# Class to reference the meals table in the database
-class Meals:
-    def __init__(self, id, date_id, meal_name):
-        self.id = id
-        self.date_id = date_id
-        self.meal_name = meal_name
-
-    def __str__(self):
-        return f"{self.meal_name}"
