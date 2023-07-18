304a21342e0f8d1faf591346afefe3f6ee3d4bd7
Brent Artuch <115948736+Artuk009@users.noreply.github.com>
2023-07-18 00:30:22 -0400

    Updated test to work with new class structure

--- Console_Application/tests/test_client_server_connection.py
+++ Console_Application/tests/test_client_server_connection.py
@@ -1,16 +1,16 @@
 import unittest
-from connection import client_server_connection as cdc
+from connection.client_server_connection import ConnectionCredentials, Connection
 
 
 class TestConnection(unittest.TestCase):
 
-    def test_user_password(self):
-        username, password = cdc.get_user_and_password()
-        assert username == 'diet_db_editor'
-        assert password == 'diet_db_editor_password'
+    def test_ConnectionCredentials(self):
+        credentials = ConnectionCredentials()
+        assert credentials.username == 'diet_db_editor'
+        assert credentials.password == 'diet_db_editor_password'
 
     def test_connection(self):
-        connection = cdc.get_connection('diet_db_editor', 'diet_db_editor_password')
+        connection = Connection('diet_db_editor', 'diet_db_editor_password').get_connection()
         assert connection.is_connected() is True
 
 
