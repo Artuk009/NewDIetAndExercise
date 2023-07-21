6d29376eed659d4785c2c9fe8ba7fee9847a511f
Brent Artuch <115948736+Artuk009@users.noreply.github.com>
2023-07-21 00:40:26 -0400

    Test are now dynamic so that they will pass without manual changes in code.

--- Console_Application/tests/test_foods.py
+++ Console_Application/tests/test_foods.py
@@ -1,33 +1,54 @@
+import pytest
 import unittest
 from unittest.mock import patch
-from diet_db import foods as f
-import connection.client_server_connection as csc
+from diet_db.foods import FoodsTable, FoodCount, Foods, FoodMasterList
+from connection.client_server_connection import ConnectionCredentials, Connection
 
 
 class FoodsTestCase(unittest.TestCase):
 
-    def test_foods_init(self):
-        test_food = f.Foods(1, 1, "apple", 1, {"fats": 95, "carbs": 25, "calories": 0.5, "proteins": 0.3})
-        assert test_food.food_id == 1
-        assert test_food.meal_id == 1
-        assert test_food.food_name == "apple"
-        assert test_food.servings == 1
-        assert test_food.nutrition_info == {"fats": 95, "carbs": 25, "calories": 0.5, "proteins": 0.3}
+    # Get connection to the database
+    pytest.credentials = ConnectionCredentials()
+    pytest.connection = Connection(
+        pytest.credentials.get_username(),
+        pytest.credentials.get_password()
+    ).get_connection()
 
-    def test_get_latest_food_entry(self):
-        pass
+    # Initialize the foods table with large number of entries for count test.
+    pytest.foods_table = FoodsTable(pytest.connection)
+    pytest.foods_table.get_foods_table(pytest.connection, 999999999)
 
-    def test_get_food_list_master(self):
-        user, password = csc.get_user_and_password()
-        connection = csc.get_connection(user, password)
-        food_list_master = f.get_food_list_master(connection)
-        assert food_list_master[0] == "10 Traditional Wings"
+    def test_get_foods_table(self):
+
+        """Tests that the first entry in the table matches the database"""
+
+        cursor = pytest.connection.cursor()
+        query = """
+        SELECT f.id, d.date, m.meal, f.food_name, f.servings, f.nutrition_info->'$.carbs' AS carbs, 
+        f.nutrition_info->'$.fats' AS fats, f.nutrition_info->'$.proteins' AS proteins,
+        f.nutrition_info->'$.calories' AS calories
+        FROM meals m
+        INNER JOIN foods_json f on f.meal_id = m.id
+        INNER JOIN dates_2023 d on d.id = m.date_id
+        ORDER BY f.id DESC LIMIT 1;"""
+        cursor.execute(query)
+        latest_food = cursor.fetchone()
+        cursor.close()
+        self.assertEqual(pytest.foods_table.foods_table[0], latest_food)
+
+    def test_get_food_master_list(self):
+
+        """Tests that the first entry in the food master list matches the database"""
+
+        food_master_list = FoodMasterList(pytest.connection).get_food_master_list()
+        assert food_master_list[0][1] == "Oatmeal"
 
     def test_get_current_foods_count(self):
-        user, password = csc.get_user_and_password()
-        connection = csc.get_connection(user, password)
-        current_foods_count = f.get_current_foods_count(connection)
-        assert current_foods_count == 240
+
+        """Tests that the current foods count matches the database"""
+
+        current_foods_count = FoodCount(pytest.connection).get_food_count()
+        assert current_foods_count == len(pytest.foods_table.foods_table)
 
 
 if __name__ == '__main__':
--- Console_Application/tests/test_meals.py
+++ Console_Application/tests/test_meals.py
@@ -1,24 +1,68 @@
+import pytest
 import unittest
 from unittest.mock import patch
-from diet_db import meals as m
+from connection.client_server_connection import ConnectionCredentials, Connection
+from diet_db.meals import MealsTable, SetMeal, Meals
+from diet_db.dates import DatabaseDateID
 
 
 class MyTestCase(unittest.TestCase):
+
+    # Get connection to the database
+    pytest.credentials = ConnectionCredentials()
+    pytest.connection = Connection(
+        pytest.credentials.get_username(),
+        pytest.credentials.get_password()
+    ).get_connection()
+
+    # Get the latest date id for testing
+    pytest.latest_date_id = DatabaseDateID(pytest.connection).get_latest_date_id()
+
+    # Initialize the meals table
+    pytest.meals_table = MealsTable(pytest.connection)
+    pytest.meals_table.get_meals_table(pytest.latest_date_id)
+
+    # Initialize the meal
+    pytest.meal = SetMeal()
+
+    def test_get_meals_table(self):
+
+        """Tests that the first entry in the table matches the database"""
+
+        cursor = pytest.connection.cursor()
+        query = "SELECT * FROM meals ORDER BY id DESC LIMIT 1"
+        cursor.execute(query)
+        latest_meal = cursor.fetchone()
+        cursor.close()
+        self.assertEqual(pytest.meals_table.meals_table[3], latest_meal)
+
     @patch('builtins.input', return_value="1")
     def test_set_breakfast(self, mock_input):
-        self.assertEqual(m.set_meal(), "Breakfast")
+
+        """Tests that the meal is set to breakfast"""
+
+        self.assertEqual(pytest.meal.set_meal(), "Breakfast")
 
     @patch('builtins.input', return_value="2")
     def test_set_lunch(self, mock_input):
-        self.assertEqual(m.set_meal(), "Lunch")
+
+        """Tests that the meal is set to lunch"""
+
+        self.assertEqual(pytest.meal.set_meal(), "Lunch")
 
     @patch('builtins.input', return_value="3")
     def test_set_dinner(self, mock_input):
-        self.assertEqual(m.set_meal(), "Dinner")
+
+        """Tests that the meal is set to dinner"""
+
+        self.assertEqual(pytest.meal.set_meal(), "Dinner")
 
     @patch('builtins.input', return_value="4")
     def test_set_snack(self, mock_input):
-        self.assertEqual(m.set_meal(), "Post-Workout")
+
+        """Tests that the meal is set to snack"""
+
+        self.assertEqual(pytest.meal.set_meal(), "Post-Workout")
 
 
 if __name__ == '__main__':
