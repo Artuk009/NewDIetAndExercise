2f1c82c81dbff2db0311187a85de2bc8dcdf82f1
Brent Artuch <115948736+Artuk009@users.noreply.github.com>
2023-07-21 00:39:19 -0400

    Updated docstrings and formatting

--- Console_Application/diet_db/meals.py
+++ Console_Application/diet_db/meals.py
@@ -1,31 +1,46 @@
-def get_meals_table(connection, date_id):
-    cursor = connection.cursor()
-    query = "SELECT * FROM meals WHERE date_id = %s"
-    cursor.execute(query, (date_id,))
-    meals_table = cursor.fetchall()
-    cursor.close()
-    return meals_table
+class MealsTable:
+
+    """Class to create a meals table object"""
+
+    def __init__(self, connection):
+        self.connection = connection
+        self.meals_table = []
+
+    def get_meals_table(self, date_id):
+        cursor = self.connection.cursor()
+        query = "SELECT * FROM meals WHERE date_id = %s"
+        cursor.execute(query, (date_id,))
+        self.meals_table = cursor.fetchall()
+        cursor.close()
+        return self.meals_table
 
 
-def set_meal():
+class SetMeal:
 
-    """Select the meal of the day"""
+    """Class to set the meal of the day"""
 
-    meal_of_day = input("Enter the meal of the day: [1]Breakfast [2]Lunch [3]Dinner [4]Post-Workout\n")
+    def __init__(self):
+        pass
 
-    meal = ''
-    if meal_of_day == '1':
-        meal = 'Breakfast'
-    elif meal_of_day == '2':
-        meal = 'Lunch'
-    elif meal_of_day == '3':
-        meal = 'Dinner'
-    elif meal_of_day == '4':
-        meal = 'Post-Workout'
-    else:
-        print("Invalid input. Please try again.")
-        set_meal()
-    return meal
+    def set_meal(self):
+
+        """Select the meal of the day"""
+
+        meal_of_day = input("Enter the meal of the day: [1]Breakfast [2]Lunch [3]Dinner [4]Post-Workout\n")
+
+        meal = ''
+        if meal_of_day == '1':
+            meal = 'Breakfast'
+        elif meal_of_day == '2':
+            meal = 'Lunch'
+        elif meal_of_day == '3':
+            meal = 'Dinner'
+        elif meal_of_day == '4':
+            meal = 'Post-Workout'
+        else:
+            print("Invalid input. Please try again.")
+            self.set_meal()
+        return meal
 
 
 class Meals:
